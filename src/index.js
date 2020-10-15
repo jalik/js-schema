9ad8aab95cd456caa9b59a887e6f7efa98fcb6a5
Karl Stein <karl.stein.pro@gmail.com>
2020-10-14 16:06:53 -1000

    Changed `main` to use index.js in package.json

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2020 Karl STEIN
+ */
+
+import Schema from './Schema';
+
+export Schema from './Schema';
+export SchemaField from './SchemaField';
+
+export default Schema;
